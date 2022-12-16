305dd59b98e390f84d3b013d8b84a1e6aea7fdba
Johan Walter <walter.johan25@gmail.com>
2022-12-16 15:31:58 +0100

    exo done

--- app/controllers/parties_controller.rb
+++ app/controllers/parties_controller.rb
@@ -1,7 +1,24 @@
 class PartiesController < ApplicationController
-  def show
+  before_action :authenticate_user!
+
+  def new
+    # affiche une nouvelle grille et un formulaire
+    @ten_letters_list = (0...10).map { ('a'..'z').to_a[rand(26)] }
   end
 
-  def create
+  def results
+    @word = params[:word].upcase
+    @ten_letters_list = params[:letters]
+    @score_results = included?(@word, @ten_letters_list)
+    if @score_results == true
+      "You score is: #{@word.length}"
+    else
+      false
+    end
   end
+
+  def included?(word, letters)
+    word.chars.all? { |letter| word.count(letter) <= letters.count(letter) }
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
-  get 'parties/show'
-  get 'parties/create'
   devise_for :users
+
   root to: "pages#home"
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  # Defines the root path route ("/")
-  # root "articles#index"
+
+  get 'parties/new'
+  get 'parties/show'
+
+  post 'results', to: 'parties#results'
+
 end
